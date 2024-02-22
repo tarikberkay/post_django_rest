b831e5e4af5bd02ed4a273bcd5052de7f49a9bda
tarikberkay <altunkanattarikberkay@gmail.com>
2024-02-22 09:40:16 +0300

    swagger added.

--- blog_rest/blog_rest/settings.py
+++ blog_rest/blog_rest/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'posts',
     'rest_framework',
+    'drf_spectacular',
     'posts.apps.PostsConfig',
     
 ]
@@ -126,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'BLOG REST',
+    'DESCRIPTION': 'BLOG REST API PROJECTS',
+    'VERSION': '1.0.0',
+}
